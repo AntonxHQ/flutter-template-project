c9ebe618c09744a6672bcd314c6f9460a93fc780
ZeeshanSalem <zeeshansaleem729@gmail.com>
2021-02-26 16:05:21 +0500

    Facebook Auth is done

--- lib/core/services/auth_services.dart
+++ lib/core/services/auth_services.dart
@@ -1,6 +1,7 @@
 import 'package:antonx/core/models/app_user.dart';
 import 'package:antonx/core/models/custom_auth_result.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import 'auth_exceptions_services.dart';
@@ -159,43 +160,56 @@ class AuthService {
     return customAuthResult;
   }
 
-//  @override
-//  Future<CustomAuthResult> loginWithFacebook() async {
-//    //Todo: Register app with facebook and do sdk settings.
-//    try {} catch (e) {}
-//  }
 
-//  ///
-//  /// Google SignIn
-//  ///
-//  @override
-//  Future<CustomAuthResult> loginWithGoogle() async {
-//    //Todo: Do settings in the Google cloud for 0Auth Credentials
-//    try {
-//      final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-//      final GoogleSignInAuthentication googleAuth =
-//          await googleUser.authentication;
-//      final AuthCredential credential = GoogleAuthProvider.getCredential(
-//        accessToken: googleAuth.accessToken,
-//        idToken: googleAuth.idToken,
-//      );
-//      final authResult = await _auth.signInWithCredential(credential);
-//      if (authResult.user != null) {
-//        customAuthResult.status = true;
-//        customAuthResult.user = authResult.user;
-//
-//        //Todo: Create Account in Database
-//      } else {
-//        customAuthResult.status = false;
-//        customAuthResult.errorMessage = 'An undefined Error happened.';
-//      }
-//    } catch (e) {
-//      customAuthResult.status = false;
-//      customAuthResult.errorMessage =
-//          AuthExceptionHandler.generateExceptionMessage(e);
-//    }
-//    return customAuthResult;
-//  }
+
+  ///
+  /// This Function for to login With Facebook
+  ///
+  Future<CustomAuthResult> signInWithFacebook() async{
+    print("@AuthService signInWithFacebook");
+    try{
+      final AccessToken accessToken = await FacebookAuth.instance.login();
+
+      final FacebookAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(accessToken.token);
+
+      if(accessToken != null){
+        final userData = await FacebookAuth.instance.getUserData();
+        appUser.email = userData["email"];
+        appUser.name = userData["name"];
+        appUser.imageUrl = userData["picture"]["data"]["url"];
+      }
+
+
+
+      final credentials = await _auth.signInWithCredential(facebookAuthCredential);
+
+      if (credentials.user == null) {
+        customAuthResult.status = false;
+//        customAuthResult.errorCode = "Undefined";
+        customAuthResult.errorMessage = 'An undefined Error happened.';
+        return customAuthResult;
+      }
+      print("User Credential Id" + credentials.user.uid);
+      appUser.id = credentials.user.uid;
+
+      ///
+      /// Check is user is new one then add user data to firebase user Collection
+      ///
+      if(credentials.additionalUserInfo.isNewUser){
+        await _dbService.registerUser(appUser); }
+
+      print("After ---------------");
+      print(credentials.user.uid);
+      print(credentials.user.email);
+
+    }on FacebookAuthException catch(e){
+      customAuthResult.status = false;
+//      customAuthResult.errorCode =  e.errorCode;
+      customAuthResult.errorMessage = AuthExceptionsService.generateExceptionMessage(e);
+    }
+    return customAuthResult;
+  }
+
 
   Future<void> logout({id}) async {
 //    if (id != null) FirebaseDatabaseService().updateFcmToken(null, id);
